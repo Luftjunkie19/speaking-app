65455d676cc8b3b88deb594e947c4ea6f9abcb00
Łukasz Borkowski <krawczyk0606@gmail.com>
2023-08-01 18:27:18 +0100

    fix issues with tiles

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import "./index.css";
 import React from "react";
 
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 
 import App from "./App";
 import FormContextProvider from "./reducer/FormContext";
+import { stoted } from "./reducer/Stored";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <FormContextProvider>
-      <App />
+      <Provider store={stoted}>
+        <App />
+      </Provider>
     </FormContextProvider>
   </React.StrictMode>
 );
